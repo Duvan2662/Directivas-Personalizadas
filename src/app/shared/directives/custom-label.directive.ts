9ddbfd872c01476364292286f291da4b0651bdd2
Duvan <duvanriano2626@gmail.com>
2024-08-24 15:11:20 -0500

    Se empieza crear propiedades y metodos en la directiva, ademas se le envian propiedades @Input para que cambie el color del texto segun el color que le mandamos

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]',
@@ -6,16 +6,29 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit{
 
   private htmlElement?:ElementRef<HTMLElement>;
+  private _color:string = 'red';
+
+  @Input() set color(value:string){
+    this._color = value;
+    this.setStyle();
+  }
 
   constructor( private elemento: ElementRef<HTMLElement> ) {
-    console.log('Contructor de la directiva');
-    console.log(elemento);
-
+    // console.log('Contructor de la directiva');
+    // console.log(elemento);
     this.htmlElement = elemento;
-
   }
   ngOnInit(): void {
-    console.log('Directiva NgOnInit');
+    // console.log('Directiva NgOnInit');
+    this.setStyle();
+  }
+
+
+  public setStyle():void{
+    if (!this.htmlElement) {
+      return
+    }
+    this.htmlElement!.nativeElement.style.color = this._color;
   }
 
 }
